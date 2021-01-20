c6e2a7acda71a3757007bd117f1cb2ba54e7099b
Tempusiosly <SlubDamage@yandex.ru>
2021-01-20 21:08:57 +0300

    added projectGit.cpp

--- projectGit.cpp
+++ projectGit.cpp
@@ -1,4 +1,20 @@
-//
-// Created by Slub Damage on 20.01.2021.
-//
+#include <iostream>
 
+int main() {
+    int targetHeight;
+    int startHeight;
+    int growth;
+    int ate;
+
+    std::cout << "Введите целевую высоту бамбука: ";
+    std::cin >> targetHeight;
+    std::cout << "Введите рост саженца: ";
+    std::cin >> startHeight;
+    std::cout << "Введите дневной рост бамбука: ";
+    std::cin >> growth;
+    std::cout << "Введите ночное поедание гусеницами: ";
+    std::cin >> ate;
+
+    int day = ((targetHeight - startHeight - growth) / (growth - ate)) + 1;
+    std::cout << "Через " << day << " полных дней высота бамбука будет " << targetHeight << " сантиметров. Если это >= 3 метра, то его можно рубить";
+}
